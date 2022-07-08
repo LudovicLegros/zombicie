18bc21fc2dfd8e7c6547ddc7146146a4f7641b62
Ludovic Legros <contact@ludoviclegros.fr>
2022-07-08 02:42:07 +0200

    Possibility to add survivant in profil

--- src/Controller/SurvivantsController.php
+++ src/Controller/SurvivantsController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 
 use App\Entity\Race;
+use App\Entity\Profil;
+use App\Entity\TableGame;
 use App\Entity\SurvivantFilter;
 use App\Form\SurvivantFilterType;
+use App\Repository\ProfilRepository;
 use App\Repository\SurvivantRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +19,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SurvivantsController extends AbstractController
 {
     #[Route('/survivants', name: 'survivants')]
-    public function index(SurvivantRepository $repository, Request $request): Response
+    #[Route('/survivantsadd/{id}', name:'survivant_route')]
+    public function index(SurvivantRepository $repository,ProfilRepository $repo, Request $request,Profil $profil = null): Response
     {   
         $survivantFilter = new SurvivantFilter;
         $form = $this->createForm(SurvivantFilterType::class, $survivantFilter);
@@ -24,10 +28,60 @@ class SurvivantsController extends AbstractController
         
         //Affichage des survivants
         $survivants = $repository->getFilter($survivantFilter);
+
+        //BLOC TESSTT-----
+        // dd($profil);
+        // $allow = 0;
+        if($profil != null){
+              
+            // $tests = $repo->findBy(['tableParty' => $id]);
+            // dd($table);
+            // foreach($table->getProfils() as $guest){
+            //     // dd($guest);
+                if($profil->getPlayer()->getId() == $this->getUser()->getId()){
+            //         $allow = 1;
+            //         break;          
+                }else{
+                    return $this->redirectToRoute('survivants');
+                }
+
+            //     if($allow = 0){
+            //         return $this->redirectToRoute('survivants');
+            //     }
+            // }
+        }
+        //BLOC TESSTT-----
         
         return $this->render('survivants/survivants.html.twig', [
-            'survivants' => $survivants,
-            'form' => $form->createView(),
+            'survivants'    => $survivants,
+            'form'          => $form->createView(),
+            'profil'        => $profil,
+            // 'tableId'       => $this->getProfil(),
         ]);
     }
+
+        // #[Route('/survivants/{id}', name:'survivant_route')]
+        // public function verifSurvivant($id, ProfilRepository $repo): Response
+        // {   
+
+        //     $tests = $repo->findBy(['tableParty' => $id]);
+        //     $allow = 0;
+        //     foreach($tests as $test){
+        //         // dd($this->getUser()->getId());
+        //         if($test->getPlayer()->getId() == $this->getUser()->getId()){
+        //             $allow = 1;
+        //             break;          
+        //         }
+
+        //         if($allow = 1){
+
+        //         }else{
+        //             return $this->redirectToRoute('home');
+        //         }
+        //     }
+
+        //     return $this->render('survivants/survivants.html.twig', [
+        
+        //     ]);
+        // }
 }
--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Profil;
+use App\Entity\Survivant;
 use App\Entity\TableGame;
+use App\Repository\ProfilRepository;
+use App\Repository\SurvivantRepository;
 use App\Repository\TableGameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,11 +16,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TableController extends AbstractController
 {
     #[Route('/table/{id}', name: 'detail_table')]
-    public function index(TableGameRepository $repository, $id): Response
+    public function index(TableGameRepository $repository, TableGame $table, $id): Response
     {
-        $thistable = $repository->find($id);
+        
+        // $thistable = $repository->find($id);
         return $this->render('table/tabledetail.html.twig', [
-            'table' => $thistable,
+            'table' => $table,
+        ]);
+    }
+
+    #[Route('/tablemanage/survivant_add/{ids}/{id}', name: 'survivant_add')]
+    public function survivantAdd(Profil $profil, EntityManagerInterface $manager, SurvivantRepository $repo,$ids): Response
+    {
+        $thisTable =$profil->getTableParty()->getId();
+        $survivant = $repo->find($ids);
+
+        $profil->addProfilsurvivant($survivant);
+        
+        $manager->persist($profil);
+        $manager->flush();
+
+
+        return $this->redirectToRoute('detail_table' , [
+            'id' => $thisTable
         ]);
     }
 }
